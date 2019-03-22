d6c81273cd6258de0579237e52226552b2b9b911
AntonPython <xmastoxa@yandex.ru>
2019-03-22 16:58:05 +0300

    Урок №6

--- models/articles.php
+++ models/articles.php
@@ -1,6 +1,12 @@
 <?php
 function articles_all(){
+    $art1 = ["id"=>1, "title"=>"Title1", "date"=>"21 марта 2019", "content"=>"Content1"];
+    $art2 = ["id"=>2, "title"=>"Title2", "date"=>"22 марта 2019", "content"=>"Content2"];
         
+    $arr[0] = $arr1;
+    $arr[1] = $arr2;
+    
+    return $arr;
     }
 function articles_get($id){
         
