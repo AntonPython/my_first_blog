7531411b6dc27db49834472f0a6abf57dfa4923e
AntonPython <xmastoxa@yandex.ru>
2019-03-25 08:35:41 +0300

    6-1

---
+++ views/articles.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset=utf-8>;
+        <title>Мой первый блог</title>
+        <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    </head>
+    <body>
+        <div class="container">
+            <h1>Мой первый блог</h1>
+            <div>
+                <div class="article">
+                    <h3></h3>
+                    <em></em>
+                    <p></p>
+                </div>
+            </div>
+            <footer>
+                <p>Мой первый блог<br>Copyright &copy; 2019</p>
+            </footer>
+        </div>
+    </body>
+</html>
